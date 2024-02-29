08690df6d914a14327ab3757eab2c1c2d1feafca
werogato <alcarazguadalupe57@gmail.com>
2024-02-29 10:41:54 -0700

    se modificaron en el tercer dia

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { getLocaleEraNames } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { PrimerDiaComponent } from './primer-dia/primer-dia.component';
 import { SegundoDiaComponent } from './segundo-dia/segundo-dia.component';
+import { TercerDiaComponent } from './tercer-dia/tercer-dia.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PrimerDiaComponent, SegundoDiaComponent],
+  imports: [RouterOutlet, PrimerDiaComponent, SegundoDiaComponent, RouterModule,TercerDiaComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
+import { PrimerDiaComponent } from './primer-dia/primer-dia.component';
+import { SegundoDiaComponent } from './segundo-dia/segundo-dia.component';
+import { TercerDiaComponent } from './tercer-dia/tercer-dia.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path:'',
+        component:PrimerDiaComponent
+    },
+
+    {
+        path: 'primer-dia',
+        component: PrimerDiaComponent
+    },
+    {
+        path: 'segundo-dia',
+        component: SegundoDiaComponent
+
+    },
+    {
+        path: 'tercer-dia',
+        component: TercerDiaComponent
+    },
+
+    {
+
+        path:'**',
+        component: PrimerDiaComponent
+    }
+
+];
