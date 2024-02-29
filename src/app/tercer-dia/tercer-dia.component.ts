5bfd6a4614519820ed44c21a541772fdf74f03d2
werogato <alcarazguadalupe57@gmail.com>
2024-02-29 10:40:27 -0700

    se agrego tercer dia

---
+++ src/app/tercer-dia/tercer-dia.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tercer-dia',
+  standalone: true,
+  imports: [],
+  templateUrl: './tercer-dia.component.html',
+  styleUrl: './tercer-dia.component.scss'
+})
+export class TercerDiaComponent {
+
+}
